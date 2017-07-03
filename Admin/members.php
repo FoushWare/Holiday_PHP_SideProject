ca804b33e2d70b1c574efd3b5fcd791359d3549d
lionmoon60 <lionmoon60@yahoo.com>
2017-07-03 07:49:49 +0200

    Add code of update page

--- Admin/members.php
+++ Admin/members.php
@@ -8,80 +8,87 @@ $pageTitle="Members";
 
 if(isset($_SESSION['Username'])){
 
-include 'init.php';
-//This variable $do we will use it to split the page
-$do = isset($_GET['do']) ? $_GET['do'] : 'Manage';
+    include 'init.php';
+    //This variable $do we will use it to split the page
+    $do = isset($_GET['do']) ? $_GET['do'] : 'Manage';
 
 
 //This Step will split the Page
 if($do == 'Edit'){ //brace stat of Edit page
 
-/*****Don't forget we pass the userid with the URL to make changes to the user profile
- ***i know this can be hackable cause if i change $userid from URL i'll
- *** change setting of other user  ---but-- :) this account for admin and
- ***he/she have every privileges of him and all users*/
-$userid=isset($_GET['userid']) && is_numeric($_GET['userid']) ? intval($_GET['userid']) : 0;
+        /*****Don't forget we pass the userid with the URL to make changes to the user profile
+         ***i know this can be hackable cause if i change $userid from URL i'll
+         *** change setting of other user  ---but-- :) this account for admin and
+         ***he/she have every privileges of him and all users*/
+     $userid=isset($_GET['userid']) && is_numeric($_GET['userid']) ? intval($_GET['userid']) : 0;
 
-//SELECT All data depend of this id
-$stmt = $con->prepare("SELECT * FROM users WHERE UserID = ? LIMIT 1");
+        //SELECT All data depend of this id
+        $stmt = $con->prepare("SELECT * FROM users WHERE UserID = ? LIMIT 1");
 
-$stmt->execute(array($userid));
-$row=$stmt->fetch();
-$count= $stmt->rowCount();
+        $stmt->execute(array($userid));
+        $row=$stmt->fetch();
+        $count= $stmt->rowCount();
 if($count>0){//if the user exists it will go to the form
 ?>
 
-<div class="container">
-<form class="form-horizontal" action="?do=Update" method="POST">
-<!-- Start username Field-->
-    <div class="form-group form-group-lg">
-            <label  class="col-sm-2 control-label">UserName</label>
-            <div class="col-sm-10">
-                <input type="text" name="username" class="form-control" autocomplete="off" required="required" placeholder="Username"/>
+        <div class="container">
+        <h1 class="text-center">Edit Member</h1>
+
+        <form class="form-horizontal" action="?do=Update" method="POST">
+
+            <!-- START OF this is a hidden input to send userid to Update page so no anyone see anything :> -->
+            <input type="hidden" name="userid" value="<?php echo $userid; ?>"/>
+            <!-- End OF this is a hidden input to send userid to Update page so no anyone see anything :> -->
+        <!-- Start username Field-->
+            <div class="form-group form-group-lg">
+                    <label  class="col-sm-2 control-label">UserName</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="username" class="form-control" autocomplete="off" required="required" placeholder="Username"/>
+                    </div>
+
+            </div>
+        <!-- End username Field-->
+
+        <!-- Start Email Field-->
+            <div class="form-group form-group-lg">
+                <label  class="col-sm-2 control-label">Email</label>
+                <div class="col-sm-10">
+                    <input type="email" name="email" class="form-control" autocomplete="off" required="required" placeholder="Email"/>
+                </div>
+            </div>
+        <!-- End Email Field-->
+
+        <!-- Start Password Field-->
+            <div class="form-group form-group-lg">
+                <label  class="col-sm-2 control-label">Password</label>
+                <div class="col-sm-10">
+                <input type="hidden" name="old_password" value="<?php $row['Password']?>" />
+                    <input type="password" name="New-password" class="form-control col-sm-10" autocomplete="new-password"  placeholder="password"/>
+                </div>
+
+            </div>
+        <!-- End Password Field-->
+
+        <!-- End FullName Field-->
+
+            <div class="form-group form-group-lg">
+                <label  class="col-sm-2 control-label">FullName</label>
+                <div class="col-sm-10">
+                    <input type="text" name="full" class="form-control col-sm-10" autocomplete="off" required="required" placeholder="FullName"/>
+                </div>
+
             </div>
 
-    </div>
-<!-- End username Field-->
+        <!-- End FullName Field-->
 
-<!-- Start Email Field-->
-    <div class="form-group form-group-lg">
-        <label  class="col-sm-2 control-label">Email</label>
-        <div class="col-sm-10">
-            <input type="email" name="email" class="form-control" autocomplete="off" required="required" placeholder="Email"/>
-        </div>
-    </div>
-<!-- End Email Field-->
-
-<!-- Start Password Field-->
-    <div class="form-group form-group-lg">
-        <label  class="col-sm-2 control-label">Password</label>
-        <div class="col-sm-10">
-            <input type="password" name="password" class="form-control col-sm-10" autocomplete="new-password" required="required" placeholder="password"/>
-        </div>
-
-    </div>
-<!-- End Password Field-->
-
-<!-- End FullName Field-->
-
-    <div class="form-group form-group-lg">
-        <label  class="col-sm-2 control-label">FullName</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control col-sm-10" autocomplete="off" required="required" placeholder="FullName"/>
-        </div>
-
-    </div>
-
-<!-- End FullName Field-->
-
-    <div class="form-group form-group-lg">
-            <div class="col-sm-offset-2 col-sm-10">
-                <input type='submit' value="save" class="btn btn-primary btn-lg"/>
+            <div class="form-group form-group-lg">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <input type='submit' value="save" class="btn btn-primary btn-lg"/>
+                    </div>
             </div>
-    </div>
 
-</form>
-</div>  <!--End of container-->
+        </form>
+        </div>  <!--End of container-->
 
 <?php }//End of count>0 condition
 
@@ -123,7 +130,53 @@ echo 'Hello from Insert';
 <?php } //brace End of Insert page
 
 else if($do == 'Update'){//brace Start of Update page
-echo '<div>Hello from update</div>';
+    echo '<h1 class="text-center">Update Member</h1>';
+    /*check if the user came from the FORM with POST REQUEST
+     *and collect the data from the form to use it with SQL statement
+     * to update his/her setting
+     * */
+
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        //Then collect info from FORM :>
+        $id=$_POST['userid'];
+        $user=$_POST['username'];
+        $email=$_POST['email'];
+        $name=$_POST['full'];
+        //password trick
+        $pass=empty($_GET['New-Password']) ? $_POST['old_password'] : $_POST[New_password];
+
+        /*Form validation*/
+        $formError=array();
+        if(strlen($user)<3){$formError[]="Username can't be less than <strong>3</strong>";}
+        else if(empty($user)){$formError[]="Username can't be<strong> empty</strong>";}
+        else if(empty($email)){$formError="email can't be<strong> empty</strong>";}
+        else if(empty($name)){$formError="fullName can't be<strong> empty</strong>";}
+
+//Loop through the error
+            foreach($formError as $error){
+                echo '<div class="alert alert-danger"> . $error .</div>';
+}
+
+
+//if there is no errors we will update the user  datatbase
+
+if(empty($formError)){
+    $stmt=$con->prepare("UPDATE users SET Username=?,Email=?,Fullname=? WHERE Userid=?");
+    $stmt->execute(array($user,$email,$name,$id));
+    echo $stmt->rowCount(). 'updated';
+}
+
+
+
+
+
+}//End of REQUEST_METHOD check
+
+else{
+    echo '<div class="alert alert-danger">sorry you can not browse this page</div>';
+}
+
+
 ?>
 <?php } //brace End of Update page
 
